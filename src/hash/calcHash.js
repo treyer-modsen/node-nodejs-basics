feb106f2dc943c656dc0c290931852453459b961
treyer <nemereno_by@mail.ru>
2023-06-20 23:20:56 +0300

    feat: implement hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import * as url from "url";
+import path from "node:path";
+import crypto from "crypto";
+import { filesDir } from "../constants/files.js";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const fileName = "fileToCalculateHashFor.txt";
+const filePath = path.join(__dirname, filesDir, fileName);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = crypto.createHash("sha256");
+  const fb = fs.readFileSync(filePath);
+  hash.update(fb);
+  const hex = hash.digest("hex");
+  console.log(hex);
 };
 
 await calculateHash();
