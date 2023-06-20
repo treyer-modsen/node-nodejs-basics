7b7daf3661f85f5e1c476162145bf9c8bd8700d9
treyer <nemereno_by@mail.ru>
2023-06-20 21:49:16 +0300

    feat: implement cli subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  console.log(
+    process.argv
+      .reduce((res, el, index, arr) => {
+        if (el.startsWith("--") && arr[index + 1]) {
+          res.push(`${el.slice(2)} is ${arr[index + 1]}`);
+        }
+        return res;
+      }, [])
+      .join(", ")
+  );
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  console.log(
+    Object.entries(process.env)
+      .filter((entry) => entry[0].startsWith("RSS_"))
+      .map((entry) => `${entry[0]}=${entry[1]};`)
+      .join(" ")
+  );
 };
 
 parseEnv();
