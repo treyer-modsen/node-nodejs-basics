3cf750c240eb905da035b1793b7ed04a872e4a86
treyer <nemereno_by@mail.ru>
2023-06-24 10:13:37 +0300

    feat: implement child process subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { fork } from "child_process";
+import * as url from "url";
+import path from "path";
+import { filesDir } from "../constants/files.js";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const scriptFileName = "script.js";
+const scriptPath = path.join(__dirname, filesDir, scriptFileName);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(scriptPath, args);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["a", "b", "c", "d", 1, 2, 3, 4]);
